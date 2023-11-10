d905e38175f74cf6c2524f57a4676271d53ed935
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-11-10 06:18:23 +0100

    tic tac toe

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ void rules();
 int main()
 {
     char x, o;
+    int player = 1;
     int s;
     char a[9] = {'1',
                  '2',
@@ -41,9 +42,32 @@ int main()
             !strcmp(p1, p2) ? printf("Enter names of different players!\n\n") : FALSE;
         } while (!strcmp(p1, p2));
         decision(&x, &o, &p1);
+        boardI
     }
 }
-void rules()
+
+void rule void board(char x, char o, unsigned char *u1, unsigned char *u2, char *a)
+{
+    int i;
+    if (OS == "nt")
+        system("cls");
+    if (OS == "posix")
+        system("clear");
+    printf("\tTic-Tac-Toe\n\n");
+    printf("\n\n");
+    printf("%s:- (%c)\n%s:-  (%c)\n\n\n", u1, x, u2, o);
+
+    printf("  %c |  %c | %c\n", a[0], a[1], a[2]);
+    printf("    |    |    \n");
+    printf("----|----|----\n");
+    printf("    |    |    \n");
+    printf("  %c |  %c | %c\n", a[3], a[4], a[5]);
+    printf("    |    |    \n");
+    printf("----|----|----\n");
+    printf("  %c |  %c | %c\n", a[6], a[7], a[8]);
+    printf("    |    |    \n");
+}
+s()
 {
     char link;
     printf("\tTic-Tac-Toe\n\n");
