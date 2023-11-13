4463c29ea559b2429b1a887f1fa7ebf3c65e9e5e
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-11-13 07:21:30 +0100

    tic tac toe

--- main.c
+++ main.c
@@ -42,32 +42,20 @@ int main()
             !strcmp(p1, p2) ? printf("Enter names of different players!\n\n") : FALSE;
         } while (!strcmp(p1, p2));
         decision(&x, &o, &p1);
-        boardI
+        board(x, o, p1, p2, a);
+        do
+        {
+            player = ((player % 2) ? 1 : 2);
+            if (player == 1)
+                printf("%s Type any digit from 1-9 :- ", p1);
+            else
+                printf("%s Type any digit from 1-9:- ", p2);
+            scanf("%d", &decision);
+        }
     }
 }
 
-void rule void board(char x, char o, unsigned char *u1, unsigned char *u2, char *a)
-{
-    int i;
-    if (OS == "nt")
-        system("cls");
-    if (OS == "posix")
-        system("clear");
-    printf("\tTic-Tac-Toe\n\n");
-    printf("\n\n");
-    printf("%s:- (%c)\n%s:-  (%c)\n\n\n", u1, x, u2, o);
-
-    printf("  %c |  %c | %c\n", a[0], a[1], a[2]);
-    printf("    |    |    \n");
-    printf("----|----|----\n");
-    printf("    |    |    \n");
-    printf("  %c |  %c | %c\n", a[3], a[4], a[5]);
-    printf("    |    |    \n");
-    printf("----|----|----\n");
-    printf("  %c |  %c | %c\n", a[6], a[7], a[8]);
-    printf("    |    |    \n");
-}
-s()
+void rules()
 {
     char link;
     printf("\tTic-Tac-Toe\n\n");
@@ -107,4 +95,25 @@ bool decision(char *x, char *o, unsigned char *p1)
         *o = '0';
     }
     return 1;
+};
+void board(char x, char o, unsigned char *p1, unsigned char *p2, char *a)
+{
+    int i;
+    if (OS == "nt")
+        system("cls");
+    if (OS == "posix")
+        system("clear");
+    printf("\tTic-Tac-Toe\n\n");
+    printf("\n\n");
+    printf("%s:- (%c)\n%s:-  (%c)\n\n\n", p1, x, p2, o);
+
+    printf("  %c |  %c | %c\n", a[0], a[1], a[2]);
+    printf("    |    |    \n");
+    printf("----|----|----\n");
+    printf("    |    |    \n");
+    printf("  %c |  %c | %c\n", a[3], a[4], a[5]);
+    printf("    |    |    \n");
+    printf("----|----|----\n");
+    printf("  %c |  %c | %c\n", a[6], a[7], a[8]);
+    printf("    |    |    \n");
 };
